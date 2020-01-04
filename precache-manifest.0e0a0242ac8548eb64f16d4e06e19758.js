edb627d8205d850e613f9bac175db407d39c421c
Artyom Pogosov <artyomp@outlook.com>
2020-01-04 14:14:11 +0200

    Updates

---
+++ precache-manifest.0e0a0242ac8548eb64f16d4e06e19758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c97f5891ae9ddf30f5fdaa22dd3024",
+    "url": "/artyomP1/erez-website/index.html"
+  },
+  {
+    "revision": "a98462223f26b288ab6c",
+    "url": "/artyomP1/erez-website/static/css/main.42edc720.chunk.css"
+  },
+  {
+    "revision": "1cc3a9438ce526ad5921",
+    "url": "/artyomP1/erez-website/static/js/2.16c5ac00.chunk.js"
+  },
+  {
+    "revision": "dfeea506dedb1f8ad64e8d3829765f8f",
+    "url": "/artyomP1/erez-website/static/js/2.16c5ac00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a98462223f26b288ab6c",
+    "url": "/artyomP1/erez-website/static/js/main.1645a993.chunk.js"
+  },
+  {
+    "revision": "286164f227d83ce20f39",
+    "url": "/artyomP1/erez-website/static/js/runtime-main.c317d056.js"
+  }
+]);
